f47c920de3cea4e338c1793c05bd8d9d16e344c6
Bruno Coppi <brunocoppi130@gmail.com>
2022-09-13 16:25:38 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,10 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('Email Validator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+
+    expect(isValid).toBe(false)
+  })
+})
