04e3f648d9fb8c6999be36f9a83519250b3fd77e
Bruno Coppi <brunocoppi130@gmail.com>
2022-10-18 19:08:59 -0300

    test: ensure AccountMongoRepository returns an account on success

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
