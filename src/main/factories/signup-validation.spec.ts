89332b0658e46dbe27305f0016cdbb26cd53a034
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-02 14:54:15 -0300

    refactor: move  password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,4 +1,6 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation copy'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
 
@@ -7,12 +9,12 @@ jest.mock('../../presentation/helpers/validators/validation-composite')
 describe('SignUpValidation Factory' , () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
 
-    expect(ValidationComposite).toHaveBeenCalledWith([
-      new RequiredFieldValidation('name'),
-      new RequiredFieldValidation('email'),
-      new RequiredFieldValidation('password'),
-      new RequiredFieldValidation('passwordConfirmation')
-    ])
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
---
+++ src/presentation/helpers/validators/compare-fields-validation copy.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class CompareFieldsValidation implements Validation {
+  private readonly fieldName: string
+  private readonly fieldNameToCompareName: string
+  constructor (fieldName: string, fieldNameToCompareName: string) {
+    this.fieldName = fieldName
+    this.fieldNameToCompareName = fieldNameToCompareName
+  }
+
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldNameToCompareName]) {
+      return new InvalidParamError(this.fieldNameToCompareName)
+    }
+  }
+}
