f3250f32accb2321ce72079297a97451709c6d76
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-09 19:10:33 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAcessToken success

--- src/infra/criptography/db/mongodb/account-repository/account.spec.ts
+++ src/infra/criptography/db/mongodb/account-repository/account.spec.ts
@@ -55,4 +55,16 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const { insertedId: id } = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    await sut.updateAccessToken(id, 'any_token')
+    const account = await accountCollection.findOne({ _id: id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
