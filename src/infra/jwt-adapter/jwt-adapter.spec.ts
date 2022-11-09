24476977e322830b66dddf4ded63a2240903cfbc
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-09 15:29:32 -0300

    test: ensure JwtAdapter throws if sign throw

--- src/infra/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/jwt-adapter/jwt-adapter.spec.ts
@@ -21,4 +21,11 @@ describe('Jwt Adapter', () => {
 
     expect(accessToken).toBe('any_token')
   })
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
