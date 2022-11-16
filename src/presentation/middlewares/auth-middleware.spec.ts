ce4494ef4424d3ae330779f77c8890f65e2f60e0
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-16 20:30:42 -0300

    test: ensure AuthMiddleware return 200 if LoadAccountByTokenStub succeds

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '../helpers/http/http-helper'
+import { forbidden, ok } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
@@ -52,7 +52,16 @@ describe('Auth Middleware', () => {
   test('Should return 403 if LoadAccountByTokenStub returns null', async () => {
     const { sut, loadAccountByTokenStub } = makeSut()
     jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
-    const httpResponse = await sut.handle({})
+    const request = makeFakeRequest()
+    const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should return 200 if LoadAccountByTokenStub returns an account', async () => {
+    const { sut } = makeSut()
+    const request = makeFakeRequest()
+
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(ok({ accountId: 'valid_id' }))
   })
 })
