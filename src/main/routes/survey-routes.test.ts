52cb0e12e6824ca6a0a9cee9b85fe904dac5dc45
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-17 17:55:13 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__)
@@ -16,6 +19,9 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return 403 on add survey success', async () => {
@@ -29,6 +35,31 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      await accountCollection.insertOne({
+        name: 'Bruno',
+        email: 'brunocoppi@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const { _id } = await accountCollection.findOne({ email: 'brunocoppi@gmail.com' })
+      const accessToken = sign({ id: _id }, env.jwtSecret)
+
+      await accountCollection.updateOne({ _id },{ $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            { answer: 'Answer 1', image: 'http://image-name.com' },
+            { answer: 'Answer 2' }
+          ]
+        })
+        .expect(204)
     })
   })
 })
