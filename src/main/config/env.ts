c76e1f5df36adb49102e3a670970e361fb3c2934
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-21 19:31:02 -0300

    refactor: allows api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,5 @@
 export default {
-  // mongodb+srv://mongodb:mXAEpBDM9e1A700A@cluster0.alj9b.mongodb.net/?retryWrites=true&w=majority
-  // mXAEpBDM9e1A700A
-  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  // mongoUrl: process.env.MONGO_URL || 'mongodb+srv://mongodb-larissa-teste:mXAEpBDM9e1A700A@cluster0.alj9b.mongodb.net/?retryWrites=true&w=majority',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
