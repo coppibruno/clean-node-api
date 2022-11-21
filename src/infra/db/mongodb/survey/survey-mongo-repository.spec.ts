31a3550526f876ba72d9901df78d4d9dba9aa3c7
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-21 17:54:08 -0300

    refactor: organize SurveyMongoRepository test in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -21,18 +21,19 @@ describe('Account Mongo Repository', () => {
   const makeSut = (): SurveyMongoRepository => {
     return new SurveyMongoRepository()
   }
-
-  test('Should add a survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        { image: 'any_image', answer: 'any_answer' },
-        { answer: 'other_answer' }
-      ],
-      date: new Date()
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          { image: 'any_image', answer: 'any_answer' },
+          { answer: 'other_answer' }
+        ],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
