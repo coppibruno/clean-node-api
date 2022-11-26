86b0dbe8381114423ecf8d7d42e23c0f4c49f10a
Bruno Coppi <brunocoppi130@gmail.com>
2022-11-26 20:54:19 -0300

    test: esnure SaveSurveyResultRoute return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -2,9 +2,24 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
 let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  await accountCollection.insertOne({
+    name: 'Bruno',
+    email: 'brunocoppi@gmail.com',
+    password: '123'
+  })
+  const { _id } = await accountCollection.findOne({ email: 'brunocoppi@gmail.com' })
+  const accessToken = sign({ id: _id }, env.jwtSecret)
+
+  await accountCollection.updateOne({ _id },{ $set: { accessToken } })
+  return accessToken
+}
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -30,6 +45,25 @@ describe('Survey Result Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+    test('Should return 200 on save survey result with access token', async () => {
+      const accessToken = await makeAccessToken()
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          { answer: 'Answer 1', image: 'http://image-name.com' },
+          { answer: 'Answer 2' }
+        ],
+        date: new Date()
+      })
+      const survey: any = await surveyCollection.findOne({ question: 'Question' })
+      await request(app)
+        .put(`/api/surveys/${survey._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
